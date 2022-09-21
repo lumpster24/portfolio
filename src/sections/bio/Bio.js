72b21785574966f0611130a792400a9852189c0f
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-09-21 15:31:00 +0900

    9/21

--- src/sections/bio/Bio.js
+++ src/sections/bio/Bio.js
@@ -1,9 +1,32 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import styles from "./Bio.module.css";
 
 export default function Bio() {
   const [bioLength, setBioLength] = useState("short");
+
+  const shortBio = `I am a front-end developer who was born and raised in Hawaii.
+  Being naturally curious and creative, I have found web
+  development to be an excellent fit for me.`;
+
+  const mediumBio = `I have lived abroad for a total of 10 years and am proficient
+  in two foreign languages, the experience of which helps me
+  know exactly what I need in order to learn languages. That
+  naturally includes programming languages as well.`;
+
+  const longBio = `My hobbies include weightlifting and playing video games. I've
+  found weightlifting to be great at molding not only the
+  physical body, but the mind as well. It has taught me how
+  important keeping to a routine, consistantly improving, and
+  the quality of your work can be. Video games have taught me
+  how to look at how to improve myself rather than to blame
+  outside forces for failure.`;
+
+  const prevBioLength = useRef();
+
+  useEffect(() => {
+    prevBioLength.current = bioLength;
+  }, [bioLength]);
 
   const lengthHandler = (e) => {
     setBioLength(e.target.getAttribute("id"));
@@ -50,57 +73,36 @@ export default function Bio() {
                 <div className={styles["length-words"]}>Long</div>
               </li>
             </ul>
-            {bioLength === "short" && (
-              <div className={styles["bio-fade"]}>
-                <p>
-                  I am a front-end developer who was born and raised in Hawaii.
-                  Being naturally curious and creative, I have found web
-                  development to be an excellent fit for me.
+            {/* bio text */}
+            <div>
+              <p>{shortBio}</p>
+              {bioLength === "medium" || bioLength === "long" ? (
+                <p
+                  className={
+                    prevBioLength.current === "short" ? styles["bio-fade"] : ""
+                  }
+                >
+                  {mediumBio}
                 </p>
-              </div>
-            )}
+              ) : (
+                ""
+              )}
 
-            {bioLength === "medium" && (
-              <div className={styles["bio-fade"]}>
-                <p>
-                  I am a front-end developer who was born and raised in Hawaii.
-                  Being naturally curious and creative, I have found web
-                  development to be an excellent fit for me.
+              {bioLength === "long" && (
+                <p
+                  className={
+                    prevBioLength.current === "short" ||
+                    prevBioLength.current === "medium"
+                      ? styles["bio-fade"]
+                      : ""
+                  }
+                >
+                  {longBio}
                 </p>
-                <p>
-                  I have lived abroad for a total of 10 years and am proficient
-                  in two foreign languages, the experience of which helps me
-                  know exactly what I need in order to learn languages. That
-                  naturally includes programming languages as well.
-                </p>
-              </div>
-            )}
-
-            {bioLength === "long" && (
-              <div className={styles["bio-fade"]}>
-                <p>
-                  I am a front-end developer who was born and raised in Hawaii.
-                  Being naturally curious and creative, I have found web
-                  development to be an excellent fit for me.
-                </p>
-                <p>
-                  I have lived abroad for a total of 10 years and am proficient
-                  in two foreign languages, the experience of which helps me
-                  know exactly what I need in order to learn languages. That
-                  naturally includes programming languages as well.
-                </p>
-                <p>
-                  My hobbies include weightlifting and playing video games. I've
-                  found weightlifting to be great at molding not only the
-                  physical body, but the mind as well. It has taught me how
-                  important keeping to a routine, consistantly improving, and
-                  the quality of your work can be. Video games have taught me
-                  how to look at how to improve myself rather than to blame
-                  outside forces for failure.
-                </p>
-              </div>
-            )}
+              )}
+            </div>
           </div>
+          {/* bio pictures */}
           <div className={styles.pictures}>PICTURES</div>
         </div>
       </div>
--- src/sections/hero/Hero.js
+++ src/sections/hero/Hero.js
@@ -13,20 +13,29 @@ export default function Hero({ setSeeContactForm }) {
       <h1 className={styles.name}>Nicholas Bingham</h1>
       <h3>Web developer</h3>
       <ul className={styles["logo-box"]}>
-        <li className={styles.logo}>
-          <img src={html5Icon} alt="html logo" />
+        <li className={styles.flex}>
+          <img src={html5Icon} alt="html logo" className={styles.logo} />
+          <p className={styles.languages}>HTML</p>
         </li>
-        <li className={styles.logo}>
-          <img src={css3Icon} alt="css logo" />
+        <li className={styles.flex}>
+          <img src={css3Icon} alt="css logo" className={styles.logo} />
+          <p className={styles.languages}>CSS</p>
         </li>
-        <li className={styles.logo}>
-          <img src={javascriptIcon} alt="javascript logo" />
+        <li className={styles.flex}>
+          <img
+            src={javascriptIcon}
+            alt="javascript logo"
+            className={styles.logo}
+          />
+          <p className={styles.languages}>JavaScript</p>
         </li>
-        <li className={styles.logo}>
-          <img src={reactIcon} alt="react logo" />
+        <li className={styles.flex}>
+          <img src={reactIcon} alt="react logo" className={styles.logo} />
+          <p className={styles.languages}>React</p>
         </li>
-        <li className={styles.logo}>
-          <img src={firebaseIcon} alt="firebase logo" />
+        <li className={styles.flex}>
+          <img src={firebaseIcon} alt="firebase logo" className={styles.logo} />
+          <p className={styles.languages}>Firebase</p>
         </li>
       </ul>
       <div className={styles.pic}>Pic?</div>
