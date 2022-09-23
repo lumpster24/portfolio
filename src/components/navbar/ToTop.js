c63d83cff836e69d3675e77cf689c526c5cbfd4f
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-09-23 09:35:37 +0900

    9/23

--- src/components/navbar/ToTop.js
+++ src/components/navbar/ToTop.js
@@ -1,14 +1,27 @@
 import toTop from "./toTop.svg";
 
 import styles from "./Navbar.module.css";
+import { useEffect, useState } from "react";
 
 export default function ToTop() {
+  const [scrollY300, setScrollY300] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 100 ? setScrollY300(true) : setScrollY300(false);
+    });
+  }, [setScrollY300]);
+
   return (
-    <div className={styles["to-top"]}>
-      <a href="#hero">
-        <img src={toTop} alt="up arrow" className={styles.arrow} />
-        <p className={styles["to-top-text"]}>To top</p>
-      </a>
-    </div>
+    <>
+      {scrollY300 && (
+        <div className={styles["to-top"]}>
+          <a href="#hero">
+            <img src={toTop} alt="up arrow" className={styles.arrow} />
+            <p className={styles["to-top-text"]}>To top</p>
+          </a>
+        </div>
+      )}
+    </>
   );
 }
