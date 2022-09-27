56fff245cff30588d905344ea25b052ee913790d
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-09-27 14:33:31 +0900

    9/27

--- src/sections/hero/Hero.js
+++ src/sections/hero/Hero.js
@@ -1,10 +1,5 @@
 import { useState } from "react";
 import NavItems from "../../components/navbar/NavItems";
-import html5Icon from "./icons/html5Icon.svg";
-import css3Icon from "./icons/css3Icon.svg";
-import javascriptIcon from "./icons/javascriptIcon.svg";
-import reactIcon from "./icons/reactIcon.svg";
-import firebaseIcon from "./icons/firebaseIcon.svg";
 import Namecard from "./Namecard";
 import Projects from "../projects/Projects";
 import Bio from "../bio/Bio";
@@ -12,36 +7,12 @@ import Contact from "../contact/Contact";
 
 import styles from "./Hero.module.css";
 
-const codingLanguages = [
-  { logo: html5Icon, text: "HTML", alt: "html logo" },
-  { logo: css3Icon, text: "CSS", alt: "css logo" },
-  { logo: javascriptIcon, text: "JavaScript", alt: "javascript logo" },
-  { logo: reactIcon, text: "React", alt: "react logo" },
-  { logo: firebaseIcon, text: "Firebase", alt: "firebase logo" },
-];
-
 export default function Hero() {
   const [page, setPage] = useState("home");
 
   return (
     <div className={styles["hero-container"]}>
       <div className={styles["name-card"]}>
-        <h1 className={styles.title}>Nicholas Bingham</h1>
-
-        <h3 className={styles.occupation}>Web developer</h3>
-        <ul className={styles["logo-box"]}>
-          {codingLanguages.map((lang) => {
-            return (
-              <li key={lang.text} className={styles.flex}>
-                <img src={lang.logo} alt={lang.alt} className={styles.logo} />
-                <p className={styles.languages}>{lang.text}</p>
-              </li>
-            );
-          })}
-        </ul>
-
-        <NavItems />
-
         <div className={styles["component-container"]}>
           {page === "home" && <Namecard />}
           {page === "projects" && <Projects />}
--- src/sections/projects/Projects.js
+++ src/sections/projects/Projects.js
@@ -103,47 +103,6 @@ export default function Projects() {
               />
 
               <div className={styles["project-info-container"]}>
-                {/* title */}
-                <div>
-                  <h3 className={styles["project-title"]}>{project.title}</h3>
-
-                  {/* languages */}
-                  <ul className={styles.flex}>
-                    {project.languages.map((lang) => (
-                      <li key={lang}>
-                        <p className={styles.languages}>{lang}</p>
-                      </li>
-                    ))}
-                  </ul>
-
-                  {/* links */}
-                  <ul className={`${styles.flex} ${styles.links}`}>
-                    <li key={project.githubLink}>
-                      <a
-                        href={project.githubLink}
-                        target="_blank"
-                        rel="noreferrer noopener"
-                      >
-                        GitHub
-                      </a>
-                    </li>
-                    <li key={project.websiteLink}>
-                      <a
-                        href={project.websiteLink}
-                        target="_blank"
-                        rel="noreferrer noopener"
-                      >
-                        Website
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                {/* description */}
-                <div>
-                  <span className={styles.subtitle}>Description:</span>
-                  <p className={styles.description}>{project.description}</p>
-                </div>
-
                 {/* description */}
                 <div>
                   <span className={styles.subtitle}>Description:</span>
