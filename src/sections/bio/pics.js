7f350490348126fe19140b7a0529ea38b97cabc9
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-10-05 10:16:36 +0900

    10/5

---
+++ src/sections/bio/pics.js
@@ -0,0 +1,35 @@
+import pic1 from "./pics/pic1.jpg";
+import pic2 from "./pics/pic2.jpg";
+import pic3 from "./pics/pic3.jpg";
+import pic4 from "./pics/pic4.jpg";
+import pic5 from "./pics/pic5.jpg";
+import pic6 from "./pics/pic6.jpg";
+import pic7 from "./pics/pic7.jpg";
+import pic8 from "./pics/pic8.jpg";
+import pic9 from "./pics/pic9.jpg";
+import pic10 from "./pics/pic10.jpg";
+import pic11 from "./pics/pic11.jpg";
+import pic12 from "./pics/pic12.jpg";
+import pic13 from "./pics/pic13.jpg";
+import pic14 from "./pics/pic14.jpg";
+import pic15 from "./pics/pic15.jpg";
+
+const pics = [
+  pic1,
+  pic2,
+  pic3,
+  pic4,
+  pic5,
+  pic6,
+  pic7,
+  pic8,
+  pic9,
+  pic10,
+  pic11,
+  pic12,
+  pic13,
+  pic14,
+  pic15,
+];
+
+export default pics;
